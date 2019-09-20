f39c27afb00408887106e018c03a632279c80c76
duvld <ollejna@gmail.com>
2019-09-20 17:02:26 -0400

    voting logic through immutable maps

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,7 +1,7 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
 
-import {setEntries, next} from '../src/core';
+import {setEntries, next, vote} from '../src/core';
 
 describe('application logic', () => {
 	describe('set entries', () => {
@@ -41,6 +41,78 @@ describe('application logic', () => {
 
 		});
 
+		it('creates a tally for the voted entry', () => {
+			const state = Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 days later')
+				}),
+				entires: List()
+			});
+			const nextState = vote(state, 'Trainspotting');
+
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 days later'),
+					tally: Map({
+						'Trainspotting' : 1
+					})
+				}),
+				entires: List()
+			}));
+		});
+
+		it('adds to existing tally for voted entry', () => {
+			const state = Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 days later'),
+					tally: Map({
+						'Trainspotting': 3,
+						'28 days later': 2
+					})
+				}),
+				entries: List()
+			});
+			const nextState = vote(state, 'Trainspotting');
+
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 days later'),
+					tally: Map({
+						'Trainspotting': 4,
+						'28 days later': 2
+					})
+				}),
+				entries: List()
+			}));
+
+		});
+
 	});
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
